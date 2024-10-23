ed12389585943057fb2b428d2b4e5bc5baae8bb4
alexander <molokan2007@mail.ru>
2024-10-24 00:01:26 +0300

    Add files via upload

---
+++ 9_3.py
@@ -0,0 +1,10 @@
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (abs(len(x[0]) - len(x[1])) for x in list(zip(first , second)) if len(x[0])!=len(x[1]))
+
+second_result = (len(first[i])==len(second[i]) for i in range(len(first)) if len(first)==len(second))
+
+print(list(first_result))
+print(list(second_result))
